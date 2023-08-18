8c86c6d1576a9030dada6540950bfde50751d21d
camilabersan <camilabersan.564@gmail.com>
2023-08-18 10:16:20 -0300

    feat: create classes

---
+++ assets/js/script.category-products.js
@@ -0,0 +1,39 @@
+class Category{
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        this.products = [];
+    }
+}
+
+class Product{
+    constructor(id, name, price, category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+
+class CategoryService{
+    constructor(){
+        this.categories = [];
+        this.nextCategoryId= 1;
+    }
+
+    addCategory(name){
+        const id = this.nextCategoryId;
+        this.nextCategoryId++;
+
+        const category = new Category(id, name);
+        this.categories.push(category);
+    }
+    
+}
+
+const categoriesList = new CategoryService();
+
+function createCategory(){
+    
+}
+
